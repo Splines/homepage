82a3725f4aea2b75997edd36cb9ef2f2fa2ab7da
Splines <info@splines.me>
2026-09-07 20:24:39 +0200

    Remove dummy Vite entrypoint

--- assets/vite/entrypoints/application.js
+++
@@ -1,3 +0,0 @@
-// Vite entrypoint. Import your frontend modules here and reference this file
-// from a Jekyll layout with `{% vite_javascript_tag 'application' %}`.
-console.log("Vite ⚡️ Jekyll");
